a06119b2e7d8145f6446bc2f78752d5b637fb475
alandlan <alanhorcrux@gmail.com>
2023-10-05 19:31:12 -0300

    config summary

--- 03-dt-money/src/pages/transactions/index.tsx
+++ 03-dt-money/src/pages/transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/header"
+import { Summary } from "../../components/summary"
 
 export function Transaction(){
     return(
         <>
             <Header />
+            <Summary />
             <div>
                 <h1>Transaction</h1>
             </div>
