b5ad68dcce2553d9e54c8f81faeda26d1761eb6b
alandlan <alanhorcrux@gmail.com>
2023-10-05 19:15:17 -0300

    config header

--- 03-dt-money/src/App.tsx
+++ 03-dt-money/src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyles } from "./styles/global";
+import { Transaction } from "./pages/transactions";
 
 export function App() {
 	return (
 		<ThemeProvider theme={defaultTheme}>
 			<GlobalStyles />
-			<h1> Hello </h1>
+			<Transaction />
 		</ThemeProvider>
 	);
 }
---
+++ 03-dt-money/src/pages/transactions/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../../components/header"
+
+export function Transaction(){
+    return(
+        <>
+            <Header />
+            <div>
+                <h1>Transaction</h1>
+            </div>
+        </>
+    )
+}
